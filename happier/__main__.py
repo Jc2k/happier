8721ff999a03e7b2f553fa2fb0d568c38a02d815
John Carr <john.carr@unrouted.co.uk>
2020-04-23 11:39:09 +0100

    Start flushing out some skaffolding

--- happier/__main__.py
+++ happier/__main__.py
@@ -5,19 +5,25 @@ import logging
 
 import asyncws
 import confuse
+import asyncclick as click
 
 from .connection import Connection
 from .resources.area import Area
 
 logger = logging.getLogger(__name__)
 
+from functools import wraps
 
-async def main():
-    logging.basicConfig(level=logging.DEBUG)
+def coro(f):
+    # Click doesn't support async properly, which is annoying
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
 
-    config = confuse.Configuration("happier", __name__)
-    logger.debug("Config directory is: %s", config.config_dir())
+    return wrapper
 
+
+async def get_connection(config):
     host = config["default"]["host"].get(str)
     port = config["default"]["port"].get(int)
     access_token = config["default"]["access_token"].get(str)
@@ -25,18 +31,70 @@ async def main():
     connection = Connection(host, port, access_token)
     await connection.connect()
 
-    area = Area(connection, name="Test 1")
-    await area.delete()
+    return connection
 
-    print(await area.get_remote())
-    await area.create()
-    print(await area.get_remote())
-    await area.delete()
-    print(await area.get_remote())
 
-    await connection.close()
+@click.group()
+@click.pass_context
+async def main(ctx):
+    logging.basicConfig(level=logging.DEBUG)
+
+    config = confuse.Configuration("happier", __name__)
+    logger.debug("Config directory is: %s", config.config_dir())
+
+    ctx.ensure_object(dict)
+    ctx.obj['config'] = config
+
+
+@main.command()
+@click.argument("type")
+@click.argument("name")
+@click.pass_context
+async def get(ctx, type, name):
+    """Describe a Home Assistant resource as a manifest."""
+    connection = await get_connection(ctx.obj['config'])
+    try:
+        area = Area(connection, name=name)
+        state = await area.get_remote()
+        if not state:
+            click.secho(f"Object {type}/{name} could not be found", fg="red")
+            return
+        click.echo(state)
+
+    finally:
+        await connection.close()
+
+
+@main.command()
+@click.pass_context
+async def apply(ctx):
+    """Apply a Home Assistant manifest"""
+    connection = await get_connection(ctx.obj['config'])
+    try:
+        area = Area(connection, name="Test 1")
+
+        if not await area.get_remote():
+            logger.info("Creating area %s", "Test 1")
+            await area.create()
+    finally:
+        await connection.close()
+
+
+@main.command()
+@click.argument("type")
+@click.argument("name")
+@click.pass_context
+async def delete(ctx, type, name):
+    """Delete a Home Assistant resource"""
+    connection = await get_connection(ctx.obj['config'])
+    try:
+        area = Area(connection, name=name)
+        if await area.delete():
+            click.secho(f"{type}/{name} was removed.", fg="green")
+            return
+    finally:
+        await connection.close()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
-
+    main(_anyio_backend="asyncio")
